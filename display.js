985ad9ed6ee269f58b007db260edf7c2c301f647
Yana <kuziavra@gmail.com>
2020-10-12 19:36:28 -0500

    Animation (mostly working)

--- display.js
+++ display.js
@@ -49,10 +49,16 @@ function prepare_next_step() {
     let highlight_color = ''
     let highlight_elems = []
     let active_elem = $('#expression .expr').last() // By default, the active (clickable) element is the whole previous expression
+    let flying_elem = null
+    let flyto_elem = null
     if(_message.startsWith('Replaced')) {
         highlight_color = 'antiquewhite'
         highlight_elems = [ft] // the replaced  token
         active_elem = ft
+        symbol_name = _message.split("'")[1] // horrible hack
+        console.log(symbol_name)
+        flyto_elem = ft
+        flying_elem = $(`#${symbol_name}`)
     }
     else if(_message.startsWith('Applied')) {
         highlight_color = 'lightgreen'
@@ -61,13 +67,17 @@ function prepare_next_step() {
             ft, // The replaced lambda
             active_elem
         ]
+        flying_elem = active_elem
+        flyto_elem = ft
     }
 
     next_step = {
         message: _message,
         highlight_color: highlight_color,
         highlight_elems: highlight_elems,
-        active_elem: active_elem
+        active_elem: active_elem,
+        flying_elem: flying_elem,
+        flyto_elem: flyto_elem
     }
 
     // set up next clickable element, OR activate next step right away if it's a symbol replacement
@@ -81,15 +91,44 @@ function prepare_next_step() {
 
 function show_next_step(){
     console.log(next_step)
-    //highlght appropriate elements from previous step
-    for(el of next_step.highlight_elems)  {
-        el.css('background-color', next_step.highlight_color)
-    }
-    // show the next step and message
-    $('#expression').append(make_row(next_step.message, expr))
 
-    // prepare next step
-    prepare_next_step() 
+    if (next_step.flying_elem && next_step.flyto_elem) {
+        flying_copy = next_step.flying_elem.clone().insertBefore(next_step.flying_elem)
+            .css("position","absolute")
+            .css("background", "white")
+            .css(next_step.flying_elem.offset())
+        flying_copy.animate(next_step.flyto_elem.offset(), {
+            done:function(){
+                flying_copy.remove()
+
+                //highlght appropriate elements from previous step
+                for(el of next_step.highlight_elems)  {
+                    el.css('background-color', next_step.highlight_color)
+                }
+                // show the next step and message
+                $('#expression').append(make_row(next_step.message, expr))
+
+                // prepare next step
+                prepare_next_step() 
+            }
+        }) 
+    }
+    else {
+
+        // TODO: don't duplicate code
+
+        //highlght appropriate elements from previous step
+        for(el of next_step.highlight_elems)  {
+            el.css('background-color', next_step.highlight_color)
+        }
+        // show the next step and message
+        $('#expression').append(make_row(next_step.message, expr))
+
+        // prepare next step
+        prepare_next_step() 
+        
+    }
+
 }
 
 // Show expression to evaluate:
@@ -97,7 +136,7 @@ function show_next_step(){
 
 // Show named symbols:
 for(s_name in named_symbols) {
-    s_div = $('<div>')
+    s_div = $(`<div id=${s_name}>`)
     s_div.append(s_name + ' = ')
     s_div.append(draw_expression(named_symbols[s_name]))
     $('#symbols').append(s_div)
